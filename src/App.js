2c7615525206e402cba15803e4fd02cfff3fb4b0
jaydeep2301 <jaydeep.khatri@clementius.com>
2022-11-05 12:02:38 +0530

    Created modal

--- src/App.js
+++ src/App.js
@@ -5,11 +5,27 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
   const [allCountry, setAllCountry] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     getAllCountry();
     console.log(allCountry, "allCountry...............");
@@ -17,6 +33,7 @@ function App() {
   const getAllCountry = async () => {
     let data = await fetch("https://restcountries.com/v3.1/all");
     data = await data.json();
+    console.log(data);
     setAllCountry(data);
   };
 
@@ -34,6 +51,14 @@ function App() {
       >
         {allCountry.length > 0 &&
           allCountry.map((el, i) => {
+            let cur = "none";
+            if (el.currencies) {
+              let key = Object.keys(el.currencies);
+              if (key.length > 0) {
+                console.log(key[0]);
+                cur = el.currencies[key[0]].name;
+              }
+            }
             return (
               <>
                 <>
@@ -62,27 +87,155 @@ function App() {
                           textAlign: "left",
                         }}
                       >
-                        <span
-                          style={{
-                            fontWeight: "bold",
-                            color: "black",
-                          }}
-                        >
-                          Population:
+                        <div>
                           <span
                             style={{
-                              fontSize: "smaller",
-                              color: "grey",
+                              fontWeight: "bold",
+                              color: "black",
                             }}
                           >
-                            {el.population}
+                            Population:
+                            <span
+                              style={{
+                                fontSize: "smaller",
+                                color: "grey",
+                                marginLeft: "1%",
+                              }}
+                            >
+                              {el.population}
+                            </span>
                           </span>
-                        </span>
+                        </div>
+                        <div>
+                          <span
+                            style={{
+                              fontWeight: "bold",
+                              color: "black",
+                            }}
+                          >
+                            Region:
+                            <span
+                              style={{
+                                fontSize: "smaller",
+                                color: "grey",
+                                marginLeft: "1%",
+                              }}
+                            >
+                              {el.region}
+                            </span>
+                          </span>
+                        </div>
+                        <div>
+                          <span
+                            style={{
+                              fontWeight: "bold",
+                              color: "black",
+                            }}
+                          >
+                            Capital:
+                            <span
+                              style={{
+                                fontSize: "smaller",
+                                color: "grey",
+                                marginLeft: "1%",
+                              }}
+                            >
+                              {el.capital}
+                            </span>
+                          </span>
+                        </div>
                       </Typography>
                     </CardContent>
                     <CardActions>
-                      <Button size="small">Share</Button>
-                      <Button size="small">Learn More</Button>
+                      <Button size="small" onClick={handleOpen}>
+                        More
+                      </Button>
+                      <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                      >
+                        <Box sx={style}>
+                          <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                          >
+                            More info
+                          </Typography>
+                          <Typography
+                            id="modal-modal-description"
+                            sx={{ mt: 2 }}
+                          >
+                            <Typography
+                              variant="body2"
+                              color="text.secondary"
+                              style={{
+                                textAlign: "left",
+                              }}
+                            >
+                              <div>
+                                <span
+                                  style={{
+                                    fontWeight: "bold",
+                                    color: "black",
+                                  }}
+                                >
+                                  Native:
+                                  <span
+                                    style={{
+                                      fontSize: "smaller",
+                                      color: "grey",
+                                      marginLeft: "1%",
+                                    }}
+                                  >
+                                    {el.name.common}
+                                  </span>
+                                </span>
+                              </div>
+                              <div>
+                                <span
+                                  style={{
+                                    fontWeight: "bold",
+                                    color: "black",
+                                  }}
+                                >
+                                  Sub Region:
+                                  <span
+                                    style={{
+                                      fontSize: "smaller",
+                                      color: "grey",
+                                      marginLeft: "1%",
+                                    }}
+                                  >
+                                    {el.subregion}
+                                  </span>
+                                </span>
+                              </div>
+                              <div>
+                                <span
+                                  style={{
+                                    fontWeight: "bold",
+                                    color: "black",
+                                  }}
+                                >
+                                  Currency Name:
+                                  <span
+                                    style={{
+                                      fontSize: "smaller",
+                                      color: "grey",
+                                      marginLeft: "1%",
+                                    }}
+                                  >
+                                    {cur}
+                                  </span>
+                                </span>
+                              </div>
+                            </Typography>
+                          </Typography>
+                        </Box>
+                      </Modal>
                     </CardActions>
                   </Card>
                 </>
